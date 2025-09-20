4de49efe32057018406ae0318da29d14ff4e04cf
singhluxmi86-alt <singhluxmi86@gmail.com>
2025-09-20 18:52:58 +0530

    Add files via upload

---
+++ innova..php
@@ -0,0 +1,542 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+</head>
+	<style>
+	.bg{
+		padding:20px;
+		color:#fff;
+		background-image: linear-gradient(#1f3870, #1f3870);
+	}
+	.btn-outline-primary{
+				color:#fff;
+		border-color:#ffff;
+	}
+		.btn-outline-primary:hover{
+				color:#fff;
+			background:#1f3870;
+					border-color:#ffff;
+	}
+	.bg1{
+		color:#1f3870;
+	}
+	.bg2{
+		padding:13px;
+		margin-left:10px;
+	}
+	.navbar-toggler{
+			position:relative;
+			bottom:105px;
+			z-index:1;
+	}
+	.navbar{
+		margin-bottom:0px;
+		height:30px;
+	}
+	</style>
+	<body>
+		<div class="container-fluid">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+					<br>
+						<h1 class="bg1">INNOVA</h1>
+					</div>
+					<div class="col-sm-2">
+					<br>
+						<h4 class="bg1"> Call Today</h4><h5 class="bg1"><sub>320-12045-2334</sub></h5>
+					</div>
+					<div class="col-sm-8">
+						<nav class="navbar navbar-expand-sm bg-white navbar-light">
+						<a class="navbar-brand" href="#"></a>
+						<button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+						<span class="navbar-toggler-icon"></span>
+						</button>
+							<div class="collapse navbar-collapse" id="collapsibleNavbar">
+								<ul class="navbar-nav">
+									<li class="nav-item ">
+									<a class="nav-link bg2" href="#"><h3 class="bg1">ABOUT</h3></a>
+									</li>
+									<li class="nav-item">
+									  <a class="nav-link bg2" href="#"><h3 class="bg1">SERVICES</h3></a>
+									</li>
+									<li class="nav-item">
+									  <a class="nav-link bg2" href="#"><h3 class="bg1">PROJECTS</h3></a>
+									</li> 
+									<li class="nav-item">
+									  <a class="nav-link bg2" href="#"><h3 class="bg1">TESTMONIALS</h3></a>
+									</li>
+									<li class="nav-item">
+									  <a class="nav-link bg2" href="#"><h3 class="bg1">CONTACT</h3></a>
+									</li>	
+								</ul>
+							</div>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="demo" class="carousel slide" data-bs-ride="carousel">
+			  <!-- The slideshow/carousel -->
+			  <div class="carousel-inner">
+				<div class="carousel-item active">
+				  <img src="images/homec.jpg" alt="Los Angeles" class="d-block" style="width:100%">
+				  <div class="carousel-caption text-center">
+						<h1 class="text-white"><b>Home Construction <br> & Remodling</b></h1>
+						<br>
+						<h6 class= "text-white">IipsumLoreum Loreum Iipsum Loreum Iipsum  Loreum <br> Iipsum Loreum IipsumLoreum  Loreum Iipsum <br> IipsumLoreum Loreum Iipsum</h6>
+						<br>
+						<br>
+					<button type="button" class="btn-danger"><h4 class="text-white">LERAN MORE</h4></button>
+				  </div>
+				</div>
+				<div class="carousel-item">
+				  <img src="images/homec1.jpg" alt="Chicago" class="d-block" style="width:100%">
+				  <div class="carousel-caption text-center">
+					<h1 class="text-white"><b>Home Construction <br> & Remodling</b></h1>
+						<br>
+						<h6 class= "text-white">IipsumLoreum Loreum Iipsum Loreum Iipsum  Loreum <br> Iipsum Loreum IipsumLoreum  Loreum Iipsum <br> IipsumLoreum Loreum Iipsum</h6>
+						<br>
+						<br>
+					<button type="button" class="btn-danger"><h4 class="text-white">LERAN MORE</h4></button>
+				  </div> 
+				</div>
+				<div class="carousel-item">
+				  <img src="images/home.jpg" alt="New York" class="d-block" style="width:100%">
+				  <div class="carousel-caption">
+					<h1 class="text-white"><b>Home Construction <br> & Remodling</b></h1>
+						<br>
+						<h6 class= "text-white">IipsumLoreum Loreum Iipsum Loreum Iipsum  Loreum <br> Iipsum Loreum IipsumLoreum  Loreum Iipsum <br> IipsumLoreum Loreum Iipsum</h6>
+						<br>
+						<br>
+					<button type="button" class="btn-danger"><h4 class="text-white">LERAN MORE</h4></button>
+				  </div>  
+				</div>
+			  </div>
+			  <!-- Left and right controls/icons -->
+			  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+				<span class="carousel-control-prev-icon"></span>
+			  </button>
+			  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+				<span class="carousel-control-next-icon"></span>
+			  </button>
+			</div>
+		<div class="container-fluid bg">
+			<div class="container">
+			<div class="row">
+				<div class="col-sm-8 text-center">
+					<h2>Cost For Your Home Renovation Project</h2>
+					<p>Get Started Today and Complete Our Form To request you Free Estimate.</p>
+				</div>
+				<div class="col-sm-4">
+				<br>
+					<button type="submit"  class="btn btn-outline-primary btn-lg">FREE ESTIMATE</h3></button>
+				</div>
+			</div>
+			</div>
+		</div>
+			<div class="container-fluid bg-light">
+				<br>
+				<br>
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-6 text-center">
+							<div class="col-sm-10">
+							<div class="card">
+								<img src="images/me.jpg" height="200">
+							</div>
+							</div>
+						</div>
+						<div class="col-sm-5">
+							<h1> Who We Are</h1>
+							<div class="progress">
+							<div class="progress-bar bg-danger" style="width:10%"></div>
+							</div><br>
+							<h5 class="text-muted">
+								IipsumLoreum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum 
+								IipsumLoreum  Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum
+								IipsumLoreum  Loreum Iipsum Loreum  Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum
+								IipsumLoreum  Loreum Iipsum Loreum  Loreum Iipsum Loreum Iipsum Loreum 
+								<br>
+								<br>
+								</h5>
+									<h2>Why We Choose Us?</h2>
+									<br>
+										<div class="row">
+								<div class="col-sm-6">
+									<ul class="icon-list">
+									 <li class="text-muted"> Year Of Experience</li>
+									 <li class="text-muted"> Fully Refund</li>
+									 <li class="text-muted">  Cost Control Experts</li>
+									 <li class="text-muted">  100% Satisfication Guarnt</li>
+									</ul>
+							</div>
+								<div class="col-sm-6">
+								<ul class="icon-list">
+									 <li class="text-muted">  Free Consulation</li>
+									 <li class="text-muted">  Satisfied Customers</li>
+									 <li class="text-muted"> Project Managment</li>
+									 <li class="text-muted"> Affilated </li>
+									</ul>
+								</div>
+						</div>
+						</div>
+					</div>
+					</div>
+					<br>
+					<br>
+			  </div>
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+			  <div class="container-fluid">
+				<div class="container">
+					<h2>Our Services</h2>
+					<div class="progress">
+						<div class="progress-bar bg-danger" style="width:10%">
+						</div>
+					</div>
+					<br>
+					<br>
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="card">
+								<img src="images/homec.jpg" height="150">
+								<div class="card-body">
+									<div class="card-tittle">
+									<h2><b>New Home Construction</b></h2>
+									</div>
+									<p class="card-text text-muted"><b>IipsumLoreum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum IipsumLoreum  Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum
+								IipsumLoreum  Loreum Iipsum Loreum  Loreum Iipsum Loreum Iipsum Loreum </b></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+						<div class="card">
+								<img src="images/home.jpg" height="150">
+								<div class="card-body">
+									<div class="card-tittle">
+									<h2><b> Home Addition</b></h2>
+									</div>
+									<p class="card-text text-muted"><b>IipsumLoreum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum IipsumLoreum  Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum
+								IipsumLoreum  Loreum Iipsum Loreum  Loreum Iipsum Loreum Iipsum Loreum </b></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+						<div class="card">
+								<img src="images/bath.jpg" height="150">
+								<div class="card-body">
+									<div class="card-tittle">
+									<h2><b>Bathroom Remodels</b></h2>
+									</div>
+									<p class="card-text text-muted"><b>IipsumLoreum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum IipsumLoreum  Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum
+								IipsumLoreum  Loreum Iipsum Loreum  Loreum Iipsum Loreum Iipsum Loreum </b></p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<br>
+					<br>
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="card">
+								<img src="images/Kitchen.jpg" height="150">
+								<div class="card-body">
+									<div class="card-tittle">
+									<h2><b>Kitchen Remodels </b></h2>
+									</div>
+									<p class="card-text text-muted"><b>IipsumLoreum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum IipsumLoreum  Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum
+								IipsumLoreum  Loreum Iipsum Loreum  Loreum Iipsum Loreum Iipsum Loreum </b></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+						<div class="card">
+								<img src="images/homec1.jpg" height="150">
+								<div class="card-body">
+									<div class="card-tittle">
+									<h2><b> Windows & Doors</b></h2>
+									</div>
+									<p class="card-text text-muted"><b>IipsumLoreum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum IipsumLoreum  Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum
+								IipsumLoreum  Loreum Iipsum Loreum  Loreum Iipsum Loreum Iipsum Loreum </b></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+						<div class="card">
+								<img src="images/deck.jpg" height="150">
+								<div class="card-body">
+									<div class="card-tittle">
+									<h2><b>Decks & Porches</b></h2>
+									</div>
+									<p class="card-text text-muted"><b>IipsumLoreum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum IipsumLoreum  Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum Iipsum
+								IipsumLoreum  Loreum Iipsum Loreum  Loreum Iipsum Loreum Iipsum Loreum </b></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			 </div>
+			 <br>
+			 <br>
+			 <br>
+			 <div class="container-fluid bg-light">
+				<div class="container">
+					<h2><b>Our Works</b></h2>
+					<div class="progress">
+						<div class="progress-bar bg-danger" style="width:10%">
+						</div>
+					</div>
+					<br>
+					<br>
+					<br>
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="card">
+								<img src="images/home.jpg" height="200">
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="card">
+								<img src="images/homec.jpg" height="200">
+							</div>
+						</div>
+						
+						<div class="col-sm-4">
+							<div class="card">
+								<img src="images/homec1.jpg" height="200">
+							</div>
+						</div>
+					</div>
+					<br>
+					<br>
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="card">
+								<img src="images/Kitchen.jpg" height="200">
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="card">
+								<img src="images/deck.jpg" height="200">
+							</div>
+						</div>
+						
+						<div class="col-sm-4">
+							<div class="card">
+								<img src="images/bath.jpg" height="200">
+							</div>
+						</div>
+					</div>
+					<br>
+					<br>
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="card">
+								<img src="images/home2.jpg" height="200">
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="card">
+								<img src="images/home1.jpg" height="200">
+							</div>
+						</div>
+						
+						<div class="col-sm-4">
+							<div class="card">
+								<img src="images/Kitchen.jpg" height="200">
+							</div>
+						</div>
+					</div>
+				</div>
+			 </div>
+			 <br>
+			 <br>
+			 <br>
+		<div class="container-fluid">
+			<div class="container">
+				<h2><b>Testmonials</b></h2>
+				<div class="progress">
+					<div class="progress-bar bg-danger" style="width:10%">
+					</div>
+				</div>
+					<br>
+					<br>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="row">
+							<div class="col-sm-3">
+								<img src="images/Kitchen.jpg" height="50">
+							</div>
+							<div class="col-sm-6">
+							<p class="card-text text-muted"><b>IipsumLoreum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum IipsumLoreum  Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum lipsum </b></p>
+							<h5 class="text-dark"> ~YOHN DOE </h4>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="row">
+							<div class="col-sm-3">
+								<img src="images/deck.jpg" height="50">
+							</div>
+							<div class="col-sm-6">
+							<p class="card-text text-muted"><b>IipsumLoreum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum IipsumLoreum  Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum lipsum </b></p>
+							<h5 class="text-dark"> ~JOHNETHIAN DOE </h4>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="row">
+							<div class="col-sm-3">
+								<img src="images/deck.jpg" height="50">
+							</div>
+							<div class="col-sm-6">
+							<p class="card-text text-muted"><b>IipsumLoreum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum IipsumLoreum  Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum lipsum </b></p>
+							<h5 class="text-dark"> ~JOHN DOE </h4>
+							</div>
+						</div>
+					</div>
+				</div>
+					<br>
+					<br>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="row">
+							<div class="col-sm-3">
+								<img src="images/Kitchen.jpg" height="50">
+							</div>
+							<div class="col-sm-6">
+							<p class="card-text text-muted"><b>IipsumLoreum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum IipsumLoreum  Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum lipsum </b></p>
+							<h5 class="text-dark"> ~YOHN DOE </h4>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="row">
+							<div class="col-sm-3">
+								<img src="images/deck.jpg" height="50">
+							</div>
+							<div class="col-sm-6">
+							<p class="card-text text-muted"><b>IipsumLoreum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum IipsumLoreum  Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum lipsum </b></p>
+							<h5 class="text-dark"> ~JOHNETHIAN DOE </h4>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="row">
+							<div class="col-sm-3">
+								<img src="images/deck.jpg" height="50">
+							</div>
+							<div class="col-sm-6">
+							<p class="card-text text-muted"><b>IipsumLoreum Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum IipsumLoreum  Loreum Iipsum Loreum Iipsum Loreum Iipsum Loreum lipsum </b></p>
+							<h5 class="text-dark"> ~JOHN DOE </h4>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	    </div>
+		<br>
+		<br>
+		<br>
+		<div class="container-fluid bg">
+			<div class="container">
+				<br>
+				<br>
+				<br>
+				<br>
+				<div class="row">
+					<div class="col-sm-7">
+						<div class="row">
+							<div class="col-sm-10">
+								<h1 class="text-white"><b> Get In Touch </b></h1>
+								<div class="progress bg">
+									<div class="progress-bar bg-danger" style="width:10%">
+									</div>
+								</div>
+								<h5 class="text-white">Please Fill Out The Form below to Send us an Email and we Will get back to you as Soon as Possible.</h5>
+								<br>
+								<br>
+						<form>
+							<div class="row">
+								<div class="col-sm-6">
+									<input type="text" class="form-control" placeholder="Name" name="Name">
+								</div>
+								<div class="col-sm-6">
+									<input type="text" class="form-control" placeholder="Email" name="Email">
+								</div>
+								<label for ="Comment "></label>
+								<br>
+								<textarea class="form-control" rows="5" id="comment" name="text" placeholder ="Message"></textarea>
+								<div class="col-sm-4">
+								<br>
+								<br>
+								<button type="button" class="btn btn-outline-light btn-lg"><h4>Send Message</h4></button>
+								</div>
+							</div>
+					  </form>
+					  <br>
+					  <br>
+					  </div>
+						</div>
+					</div>
+					<div class="col-sm-5">
+						<h3 class="text-white">Contact Info</h3>
+						<br>
+						<div class="progress">
+							<div class="progress-bar bg" style="width:100%">
+							</div>
+						</div>
+						<br>
+						<br>
+						<h4 class="text-white">Address</h4>
+						<h5 class="text-white"> 4522 Calefornia st<br> San Fransico,CA 7845</h5>
+						<br>
+						<h4 class="text-white">Phone</h4>
+						<h5 class="text-white"> +1 647564574 </h5>
+						<br>
+						<h4 class="text-white">Email</h4>
+						<h5 class="text-white">Info @company.com </h5>
+					</div>
+				</div>
+				<br>
+				<br>
+				<div class="progress">
+					<div class="progress-bar bg" style="width:100%">
+						</div>
+					</div>
+					<div class="row ">
+							<div class="col-sm-12 text-center text-white">
+								<i class="bi bi-facebook fs-1 bg2"></i>
+								<i class="bi bi-twitter fs-1 bg2"></i>
+								<i class="bi bi-instagram fs-1 bg2"></i>
+								<i class="bi bi-amazon fs-1 bg2"></i>
+							</div>
+					</div>
+				</div>
+				<br>
+				<br>
+			</div>
+		</div>	 
+			<br>
+			<br>
+			<footer class="container-fluid">
+				<div class="container text-center">
+					<h5 class="text-muted">&copy; 2017 INNOVA Desigen by <span class="text-primary">Templateform</h5>
+				</div>
+				<br>
+				<br>
+			</div>
+	</body>
+</html>
